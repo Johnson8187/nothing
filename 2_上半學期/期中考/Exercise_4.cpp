213ca7c3d86a7e48cdfdd935125279076cba8fe3
林承諺 <johnsontw102060@gmail.com>
2026-04-14 17:04:42 +0800

    Refactor Exercise_4.cpp by cleaning up includes
    Removed unnecessary includes and whitespace.

--- 2_上半學期/期中考/Exercise_4.cpp
+++ 2_上半學期/期中考/Exercise_4.cpp
@@ -1,18 +1,12 @@
 // main.cpp
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 #include "Dice.h"
-
 
 using namespace std;
 
 int main() {
     srand(time(0));
-    
     DiceGame game;
     game.playRound();
     game.displayResult();
-    
-    return 0;
 }
